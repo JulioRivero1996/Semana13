0560ca44b76a0e869e832c1235e57e3d076bb774
Julio Rivero <127893457+JulioRivero1996@users.noreply.github.com>
2023-08-14 14:25:08 +0000

    nueve veinte

---
+++ src/main/js/app.js
@@ -0,0 +1,28 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { createBrowserRouter, RouterProvider } = require('react-router-dom');
+
+const PageHome = require('./pages/home');
+const PageNuevoMusico = require('./pages/nuevo-musico');
+const PageNuevoIntrumento = require('./pages/nuevo-instrumento');
+const PageNuevaBanda = require('./pages/nueva-banda');
+const PageEditarMusico = require('./pages/editar-musico');
+const PageEditarInstrumento = require('./pages/editar-instrumento');
+
+
+const router = createBrowserRouter([
+	{ path: '/', element: <PageHome /> },
+	{ path: '/nuevo-musico', element: <PageNuevoMusico /> },
+	{ path: '/nuevo-instrumento', element: <PageNuevoIntrumento /> },
+	{ path: '/nueva-banda', element: <PageNuevaBanda /> },
+	{ path: '/editar-musico/:id', element: <PageEditarMusico /> },
+	{ path: '/editar-instrumento/:id', element: <PageEditarInstrumento /> }
+]);
+
+
+ReactDOM.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+	document.getElementById('react')
+)
